fd9f7adac52cbcb501ab4f2d36cf9c25be8f1b0c
arivia20 <129972326+arivia20@users.noreply.github.com>
2023-05-05 23:12:09 +0700

    Create lpk.py

---
+++ lpk.py
@@ -0,0 +1,22 @@
+Soal = st.selectbox(
+    'Pilih soal',
+    ('Soal 1', 'Soal 2', 'Soal 3'))
+if Soal == "Soal 1":
+    st.write("titran pada standardisasi NaOH adalah...")
+    col1, col2 = st.columns (2)
+    with col1:
+        opsi1 = st.button("NaOH")
+        opsi2 = st.button("KMNO4")
+    with col2:
+        opsi3 = st.button("AgNO3")
+        opsi1 = st.button("HCL")
+    if opsi1:
+        st.write("Benar")
+        st.ballons()
+    elif opsi2:
+        st.write("Salah")
+    elif opsi3:
+        st.write("salah")
+    elif opsi1:
+        st.write("Salah")
+   

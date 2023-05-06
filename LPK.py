8dcf8466bfdb6fe794871517d6b8e214c0e761ba
arivia20 <129972326+arivia20@users.noreply.github.com>
2023-05-06 22:24:01 +0700

    Update LPK.py

--- LPK.py
+++ LPK.py
@@ -12,14 +12,14 @@ st.header('QUIZ')
 st.caption('yuk belajar')
 
 option = st.selectbox(
-    'pilih soal',
+    'pilih materi',
     ('Analisis Titrimetri', 'Kimia Organik'))
 
 
 Soal = st.selectbox(
-    'pilih soal Analisis titrimetri',
+    'Analisis titrimetri',
     ('Soal 1', 'Soal 2', 'Soal 3','Soal 4','Soal 5'))
-st.divider()
+
 if Soal == "Soal 1":
     st.write("titran pada standardisasi alkalimetri adalah...")
     col1, col2 = st.columns (2)

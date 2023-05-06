72793925981810a4289cc3a6697e963fca435776
Ekkerose16 <132033852+Ekkerose16@users.noreply.github.com>
2023-05-06 13:35:14 +0700

    Update LPK.py

--- LPK.py
+++ LPK.py
@@ -40,3 +40,22 @@ if soal == "Soal 2":
         st.balloons()
     elif opsi4:
         st.write("salah")
+
+  if soal == "Soal 3":
+    st.write("Alkil halida-alkil halida yang dapat digunakan untuk membuat 2-metil-3-siklobutilpentana dengan cara Corey-House adalah(alkil halida sesuai tahapannya)...")
+    col1, col2 = st.columns (2)
+    with col1:
+        opsi1= st.button("1-bromo-1-siklobutilpropana dan 2-bromopropana")
+        opsi2= st.button("Metilbromida dan 2-bromo-3-siklobutilpentana")
+    with col2:
+        opsi3= st.button("2-bromo-3-siklobutilpentana dan metilbromida")
+        opsi4= st.button("2-bromopropana dan 1-bromo-1-siklopropilpropana")
+    if opsi1:
+        st.write("salah")
+    elif opsi2:
+        st.write("salah")
+    elif opsi3:
+        st.write("benar")
+        st.balloons()
+    elif opsi4:
+        st.write("salah")

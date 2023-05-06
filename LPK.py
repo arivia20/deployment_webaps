33f713e25931a22fc71bbd2765d08c1bb461787b
arivia20 <129972326+arivia20@users.noreply.github.com>
2023-05-06 09:03:10 +0700

    Update LPK.py

--- LPK.py
+++ LPK.py
@@ -7,18 +7,18 @@ if Soal == "Soal 1":
     st.write("titran pada standardisasi NaOH adalah...")
     col1, col2 = st.columns (2)
     with col1:
-        opsi1 = st.button("NaOH")
+        opsi1 = st.button("HCL")
         opsi2 = st.button("KMNO4")
     with col2:
         opsi3 = st.button("AgNO3")
-        opsi4 = st.button("HCL")
+        opsi4 = st.button("NaOH")
     if opsi1:
-        st.write("Benar")
-        st.ballons()
+        st.write("Salah")
     elif opsi2:
         st.write("Salah")
     elif opsi3:
         st.write("salah")
     elif opsi4:
-        st.write("Salah")
+        st.write("Benar")
+        st.ballons()
    

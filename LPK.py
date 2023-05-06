361559f63cc4567fac24bd960e8fb4c424706654
arivia20 <129972326+arivia20@users.noreply.github.com>
2023-05-06 23:18:05 +0700

    Update LPK.py

--- LPK.py
+++ LPK.py
@@ -4,9 +4,9 @@ import streamlit as st
 import streamlit as st
 from PIL import Image
 
-image = Image.open('https://github.com/arivia20/deployment_webaps/blob/main/IMG_20230506_184224.jpg')
+image = Image.open('IMG_20230506_184224.jpg')
 
-st.image(image, caption='stevanny a','lulu','ekke','kania','pandan tsiqqa arivia')
+st.image(image, caption='stevanny','lulu','ekke','kania','pandan')
 
 with st.spinner('Wait for it 03.00'):
     time.sleep(5)

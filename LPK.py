aa9393a253f6e57f56457246f8d73bac40b60531
arivia20 <129972326+arivia20@users.noreply.github.com>
2023-05-06 16:46:28 +0700

    Update LPK.py

--- LPK.py
+++ LPK.py
@@ -14,7 +14,7 @@ st.header('Anak AKA')
 
 Soal = st.selectbox(
     'Pilih soal',
-    ('Soal 1', 'Soal 2', 'Soal 3', 'Soal 4', 'Soal 5', 'Soal 6', 'Soal 7', 'Soal 8', 'soal 9', 'Soal 10'))
+    ('Soal 1', 'Soal 2', 'Soal 3', 'Soal 4', 'Soal 5', 'Soal 6', 'Soal 7', 'Soal 8', 'Soal 9', 'Soal 10'))
 if Soal == "Soal 1":
     st.write("titran pada standardisasi alkalimetri adalah...")
     col1, col2 = st.columns (2)

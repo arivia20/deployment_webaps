e2e46d9c923e1977d34fb915ac52f30090fac3a3
arivia20 <129972326+arivia20@users.noreply.github.com>
2023-05-06 17:07:35 +0700

    Update LPK.py

--- LPK.py
+++ LPK.py
@@ -15,7 +15,7 @@ import streamlit as st
 soal = st.select_slider(
     'pilihan nomer',
     options=['1', '2', '3', '4', '5', '6', '7', '8', '9' , '10'],
-    value=('red'))
+    value=('nomer'))
 
 Soal = st.selectbox(
     'Pilih soal',

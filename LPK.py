6f248cbac9338795e10b1972fb394458a027dcc3
Ekkerose16 <132033852+Ekkerose16@users.noreply.github.com>
2023-05-07 19:00:16 +0700

    Update LPK.py

--- LPK.py
+++ LPK.py
@@ -130,23 +130,23 @@ elif Soal == "Soal 5":
         st.balloons()
 
 elif Soal == "Soal 6":
-    st.write("Alkil halida-alkil halida yang dapat digunakan untuk membuat 2-metil-3-siklobutilpentana dengan cara Corey-House adalah(alkil halida sesuai tahapannya)...")
+    st.write("Reaksi positif uji Tollens terhadap aldehih secara umum adalah...")
     col1, col2 = st.columns (2)
     with col1:
-        opsi1= st.button("1-bromo-1-siklobutilpropana dan 2-bromopropana")
-        opsi2= st.button("Metilbromida dan 2-bromo-3-siklobutilpentana")
+        opsi1= st.button("Terbentuknya larutan berwarna violet")
+        opsi2= st.button("Terbentuk endapan merah bata")
     with col2:
-        opsi3= st.button("2-bromo-3-siklobutilpentana dan metilbromida")
-        opsi4= st.button("2-bromopropana dan 1-bromo-1-siklopropilpropana")
+        opsi3= st.button("Terbentuk endapan putih")
+        opsi4= st.button("Terbentuk cermin perak")
     if opsi1:
         st.write("salah")
     elif opsi2:
         st.write("salah")
     elif opsi3:
-        st.write("benar")
-        st.balloons()
+        st.write("salah")
     elif opsi4:
-        st.write("salah") 
+        st.write("benar")
+         st.balloons()
         
 elif Soal == "Soal 7":
     st.write("Campuran reagen berikut yang menghasilkan reagen uji Tollens adalah...")

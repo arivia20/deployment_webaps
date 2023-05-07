bd4c515fc7ba4901a75b42e96b11a78f2e309f7b
kania08 <130148499+kania08@users.noreply.github.com>
2023-05-07 05:19:59 -0700

    Update LPK.py

--- LPK.py
+++ LPK.py
@@ -307,5 +307,4 @@ elif Soal == "Soal 10":
         st.write("Skor anda sekarang adalah",score)
         st.write("\n")
        
-st.write("Anda mendapat skor" + str(score) +"jawaban benar!")
 st.write("Nilai anda adalah" + str((score / 4)*100) +"%")

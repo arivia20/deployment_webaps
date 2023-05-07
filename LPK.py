7df4db61b392da70037f32aa06d1b52489d64d4a
arivia20 <129972326+arivia20@users.noreply.github.com>
2023-05-07 10:38:00 +0700

    Update LPK.py

--- LPK.py
+++ LPK.py
@@ -22,7 +22,7 @@ st.caption('yuk belajar')
 Soal = st.selectbox(
     'pilihan soal',
     ('Soal 1','Soal 2','Soal 3','Soal 4','Soal 5','Soal 6','Soal 7','Soal 8','Soal 8','Soal 9','Soal 10'))
-    'pilihan soal'
+    'pilihan materi'
     ('soal titri')
 
 

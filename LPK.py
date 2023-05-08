f4acfaacfcf5ddaaeb853d710e73c6201987bf80
arivia20 <129972326+arivia20@users.noreply.github.com>
2023-05-08 18:38:23 +0700

    Update LPK.py

--- LPK.py
+++ LPK.py
@@ -23,7 +23,7 @@ st.score = 0
 
 Soal = st.selectbox(
     'pilihan soal',
-    ("Soal 1","Soal 2","Soal 3","Soal 4","Soal 5","Soal 6","Soal 7","Soal 8","Soal 9","Soal 10",
+    ("Soal 1","Soal 2","Soal 3","Soal 4","Soal 5","Soal 6","Soal 7","Soal 8","Soal 9","Soal 10"))
 
 if Soal == "Soal 1":
     st.write("titran pada standardisasi alkalimetri adalah...")

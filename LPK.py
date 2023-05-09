bd984fb8b3ef3db600d4b590c6248d3cc76ac36a
kania08 <130148499+kania08@users.noreply.github.com>
2023-05-08 22:04:12 -0700

    Update LPK.py

--- LPK.py
+++ LPK.py
@@ -32,31 +32,31 @@ st.caption("yuk belajar")
 
 
 ## SOAL PERTAMA
-    st.write("titran pada standardisasi alkalimetri adalah...")
-    col1, col2 = st.columns (2)
-    with col1:
-        opsi1 = st.button("HCL")
-        opsi2 = st.button("KMNO4")
-    with col2:
-        opsi3 = st.button("AgNO3")
-        opsi4 = st.button("NaOH")
-    if opsi1:
-        st.write("Salah")
-        st.write("Skor anda sekarang adalah",score)
-        st.write("\n")
-    elif opsi2:
-        st.write("Salah")
-        st.write("Skor anda sekarang adalah",score)
-        st.write("\n")
-    elif opsi3:
-        st.write("salah")
-        st.write("Skor anda sekarang adalah",score)
-        st.write("\n")
-    elif opsi4:
-        st.balloons()
-        score += 1
-        st.write ("Skor anda sekarang adalah",score)
-        st.write("\n")
+st.write("titran pada standardisasi alkalimetri adalah...")
+col1, col2 = st.columns (2)
+with col1:
+    opsi1 = st.button("HCL")
+    opsi2 = st.button("KMNO4")
+with col2:
+    opsi3 = st.button("AgNO3")
+    opsi4 = st.button("NaOH")
+if opsi1:
+    st.write("Salah")
+    st.write("Skor anda sekarang adalah",score)
+    st.write("\n")
+elif opsi2:
+    st.write("Salah")
+    st.write("Skor anda sekarang adalah",score)
+    st.write("\n")
+elif opsi3:
+    st.write("salah")
+    st.write("Skor anda sekarang adalah",score)
+    st.write("\n")
+elif opsi4:
+    st.balloons()
+    score += 1
+    st.write ("Skor anda sekarang adalah",score)
+    st.write("\n")
 
 ## SOAL KEDUA
     st.write("indikator dalam standardisasi NaOH adalah...")

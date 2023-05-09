ce72185c8c5855d9b269df3a766f89a408512f86
kania08 <130148499+kania08@users.noreply.github.com>
2023-05-08 23:23:00 -0700

    Update LPK.py

--- LPK.py
+++ LPK.py
@@ -82,120 +82,116 @@ elif opsi4:
     st.write("\n")
         
 ## SOAL KETIGA
-elif Soal== "Soal 3":
-    st.write("K2Cr2O7 digunakan sebagai baku primer dalam standarisasi...")
-    col1, col2 = st.columns (2)
-    with col1:
-        opsi1= st.button("NA2S2O3 0,1N")
-        opsi2= st.button("KMnO7 0,1N")
-    with col2:
-        opsi3= st.button("Na2B407,1N")
-        opsi4= st.button("HCl 0,1N")
-    if opsi1:
-        st.write("benar")
-        st.balloons()
-        score += 1
-        st.write ("Skor anda sekarang adalah",score)
-        st.write("\n")
-    elif opsi2:
-        st.write("salah")
-        st.write("Skor anda sekarang adalah",score)
-        st.write("\n")
-    elif opsi3:
-        st.write("salah")
-        st.write("Skor anda sekarang adalah",score)
-        st.write("\n")
-    elif opsi4:
-        st.write("salah")
-        st.write("Skor anda sekarang adalah",score)
-        st.write("\n")
+st.write("K2Cr2O7 digunakan sebagai baku primer dalam standarisasi...")
+col1, col2 = st.columns (2)
+with col1:
+    opsi1= st.button("NA2S2O3 0,1N")
+    opsi2= st.button("KMnO7 0,1N")
+with col2:
+    opsi3= st.button("Na2B407,1N")
+    opsi4= st.button("HCl 0,1N")
+if opsi1:
+    st.write("benar")
+    st.balloons()
+    score += 1
+    st.write ("Skor anda sekarang adalah",score)
+    st.write("\n")
+elif opsi2:
+    st.write("salah")
+    st.write("Skor anda sekarang adalah",score)
+    st.write("\n")
+elif opsi3:
+     st.write("salah")
+     st.write("Skor anda sekarang adalah",score)
+     st.write("\n")
+elif opsi4:
+     st.write("salah")
+     st.write("Skor anda sekarang adalah",score)
+     st.write("\n")
 
 ## SOAL KEEMPAT    
-elif Soal == "Soal 4":
-    st.write("mengapa pada titrasi permanganometri tidak digunakan indikator...")
-    col1, col2 = st.columns (2)
-    with col1:
-        opsi1= st.button("karena penambahan H2SO4 sebagai pengganti indikator")
-        opsi2= st.button("karena pemanasan yang dilakukan akan menyebabkan perubahan warna")
-    with col2:
-        opsi3= st.button("karena asam oksalat bersifat auto indikator")
-        opsi4= st.button("karena KMnO4 bersifat auto indikator")
-    if opsi1:
-        st.write("salah")
-        st.write("Skor anda sekarang adalah",score)
-        st.write("\n")
-    elif opsi2:
-        st.write("salah")
-        st.write("Skor anda sekarang adalah",score)
-        st.write("\n")
-    elif opsi3:
-        st.write("salah")
-        st.write("Skor anda sekarang adalah",score)
-        st.write("\n")
-    elif opsi4:
-        st.write("benar")
-        st.balloons()
-        score += 1
-        st.write ("Skor anda sekarang adalah",score)
-        st.write("\n")
+st.write("mengapa pada titrasi permanganometri tidak digunakan indikator...")
+col1, col2 = st.columns (2)
+with col1:
+    opsi1= st.button("karena penambahan H2SO4 sebagai pengganti indikator")
+    opsi2= st.button("karena pemanasan yang dilakukan akan menyebabkan perubahan warna")
+with col2:
+    opsi3= st.button("karena asam oksalat bersifat auto indikator")
+    opsi4= st.button("karena KMnO4 bersifat auto indikator")
+if opsi1:
+    st.write("salah")
+    st.write("Skor anda sekarang adalah",score)
+    st.write("\n")
+elif opsi2:
+    st.write("salah")
+    st.write("Skor anda sekarang adalah",score)
+    st.write("\n")
+elif opsi3:
+    st.write("salah")
+    st.write("Skor anda sekarang adalah",score)
+    st.write("\n")
+elif opsi4:
+    st.write("benar")
+    st.balloons()
+    score += 1
+    st.write ("Skor anda sekarang adalah",score)
+    st.write("\n")
     
 ## SOAL KELIMA   
-elif Soal == "Soal 5":
-    st.write("Trayek pH Fenolftalein adalah...")
-    col1, col2 = st.columns (2)
-    with col1:
-        opsi1= st.button("3,1 - 4,4")
-        opsi2= st.button("6,0 - 7,6")
-    with col2:
-        opsi3= st.button("4,2 - 6,3")
-        opsi4= st.button("8,2 - 10")
-    if opsi1:
-        st.write("salah")
-        st.write("Skor anda sekarang adalah",score)
-        st.write("\n")
-    elif opsi2:
-        st.write("salah")
-        st.write("Skor anda sekarang adalah",score)
-        st.write("\n")
-    elif opsi3:
-        st.write("salah")
-        st.write("Skor anda sekarang adalah",score)
-        st.write("\n")
-    elif opsi4:
-        st.write("benar")
-        st.balloons()
-        score += 1
-        st.write ("Skor anda sekarang adalah",score)
-        st.write("\n")
+st.write("Trayek pH Fenolftalein adalah...")
+col1, col2 = st.columns (2)
+with col1:
+    opsi1= st.button("3,1 - 4,4")
+    opsi2= st.button("6,0 - 7,6")
+with col2:
+    opsi3= st.button("4,2 - 6,3")
+    opsi4= st.button("8,2 - 10")
+if opsi1:
+    st.write("salah")
+    st.write("Skor anda sekarang adalah",score)
+    st.write("\n")
+elif opsi2:
+    st.write("salah")
+    st.write("Skor anda sekarang adalah",score)
+    st.write("\n")
+elif opsi3:
+    st.write("salah")
+    st.write("Skor anda sekarang adalah",score)
+    st.write("\n")
+elif opsi4:
+    st.write("benar")
+    st.balloons()
+    score += 1
+    st.write ("Skor anda sekarang adalah",score)
+    st.write("\n")
    
 ## SOAL KEENAM
-elif Soal == "Soal 6":
-    st.write("Alkil halida-alkil halida yang dapat digunakan untuk membuat 2-metil-3-siklobutilpentana dengan cara Corey-House adalah(alkil halida sesuai tahapannya)...")
-    col1, col2 = st.columns (2)
-    with col1:
-        opsi1= st.button("1-bromo-1-siklobutilpropana dan 2-bromopropana")
-        opsi2= st.button("Metilbromida dan 2-bromo-3-siklobutilpentana")
-    with col2:
-        opsi3= st.button("2-bromo-3-siklobutilpentana dan metilbromida")
-        opsi4= st.button("2-bromopropana dan 1-bromo-1-siklopropilpropana")
-    if opsi1:
-        st.write("salah")
-        st.write("Skor anda sekarang adalah",score)
-        st.write("\n")
-    elif opsi2:
-        st.write("salah")
-        st.write("Skor anda sekarang adalah",score)
-        st.write("\n")
-    elif opsi3:
-        st.write("benar")
-        st.balloons()
-        score += 1
-        st.write ("Skor anda sekarang adalah",score)
-        st.write("\n")
-    elif opsi4:
-        st.write("salah") 
-        st.write("Skor anda sekarang adalah",score)
-        st.write("\n")
+st.write("Alkil halida-alkil halida yang dapat digunakan untuk membuat 2-metil-3-siklobutilpentana dengan cara Corey-House adalah(alkil halida sesuai tahapannya)...")
+col1, col2 = st.columns (2)
+with col1:
+    opsi1= st.button("1-bromo-1-siklobutilpropana dan 2-bromopropana")
+    opsi2= st.button("Metilbromida dan 2-bromo-3-siklobutilpentana")
+with col2:
+    opsi3= st.button("2-bromo-3-siklobutilpentana dan metilbromida")
+    opsi4= st.button("2-bromopropana dan 1-bromo-1-siklopropilpropana")
+if opsi1:
+    st.write("salah")
+    st.write("Skor anda sekarang adalah",score)
+    st.write("\n")
+elif opsi2:
+     st.write("salah")
+     st.write("Skor anda sekarang adalah",score)
+     st.write("\n")
+elif opsi3:
+     st.write("benar")
+     st.balloons()
+     score += 1
+     st.write ("Skor anda sekarang adalah",score)
+     st.write("\n")
+elif opsi4:
+     st.write("salah") 
+     st.write("Skor anda sekarang adalah",score)
+     st.write("\n")
      
 ## SOAL KETUJUH
 elif Soal == "Soal 7":
